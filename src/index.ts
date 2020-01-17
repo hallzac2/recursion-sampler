9bae8318ffd734ab56fd587194d6c353620fa62a
Zackary Hall <hallzac2@isu.edu>
2020-01-17 08:13:45 -0700

    Add module bundle

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { trampoline } from "./trampoline";
+import { memoize } from "./memoize";
+import { factorial } from "./factorial";
+import { reverseString } from "./reverse-string";
+
+export { trampoline, memoize, factorial, reverseString };
